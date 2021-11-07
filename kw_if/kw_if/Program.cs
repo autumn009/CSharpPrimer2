51493f58487631b45334108c6a2c9d52fdd011e3
Akira Kawamata <autumn@piedey.co.jp>
2021-11-07 10:57:15 +0900

    add comment

--- kw_if/kw_if/Program.cs
+++ kw_if/kw_if/Program.cs
@@ -1,21 +1,21 @@
-﻿    int a = 1;
-    int b = 2;
+﻿int a = 1;
+int b = 2;
 
-    // if文を使う・実行されないとき
-    if (a > b) Console.WriteLine("aはbより大きい。");
-    // if文を使う・実行されるとき
-    if (b > a) Console.WriteLine("bはaより大きい。");
-    // エラーにならないが間違った例
-    if (a > b) ; Console.WriteLine("aはbより大きい。(間違った例)");
-    // elseも使う
-    for (int i = 1; i < 3; i++)
+// if文を使う・実行されないとき
+if (a > b) Console.WriteLine("aはbより大きい。");
+// if文を使う・実行されるとき
+if (b > a) Console.WriteLine("bはaより大きい。");
+// エラーにならないが間違った例 (常に実行される)
+if (a > b) ; Console.WriteLine("aはbより大きい。(間違った例)");
+// elseも使う例
+for (int i = 1; i < 3; i++)
+{
+    if (i > 1)
     {
-        if (i > 1)
-        {
-            Console.WriteLine($"変数i({i})は1より大きい。");
-        }
-        else
-        {
-            Console.WriteLine($"変数i({i})は1と同じか1より小さい。");
-        }
+        Console.WriteLine($"変数i({i})は1より大きい。");
     }
+    else
+    {
+        Console.WriteLine($"変数i({i})は1と同じか1より小さい。");
+    }
+}
