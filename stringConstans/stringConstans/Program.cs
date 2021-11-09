098a87553abdc5a79fbd9ea6b7e75ead43938b84
Akira Kawamata <autumn@piedey.co.jp>
2021-11-09 09:28:33 +0900

    add stringConstams

---
+++ stringConstans/stringConstans/Program.cs
@@ -0,0 +1,11 @@
+﻿// 通常文字列
+Console.WriteLine("He said, \r\n\"GOOD!\".");
+
+// @前置
+Console.WriteLine(@"He said, 
+""GOOD!"".");
+
+// $前置
+int a = 1;
+int b = 2;
+Console.WriteLine($"{a}足す{b}は{a+b}");
