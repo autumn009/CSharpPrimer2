5fe4306e54369754e206597e203fd0e2db3626b0
Akira Kawamata <autumn@piedey.co.jp>
2021-11-11 19:00:37 +0900

    add varBasic

---
+++ varBasic/varBasic/Program.cs
@@ -0,0 +1,9 @@
+﻿// 変数を保管用の入れ物として使う
+int a = 1;
+int b = 2;
+// 何か長い処理があるとする。
+int c = a + b;
+Console.WriteLine($"{a}+{b}={c}");
+// 変数を書き換えながら使う
+for (int i = 0; i < 10; i++) Console.Write(i);
+Console.WriteLine();
