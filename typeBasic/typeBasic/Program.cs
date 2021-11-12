b5dcbcb71b09fdb8b8d68385e95a746a943df1db
Akira Kawamata <autumn@piedey.co.jp>
2021-11-12 10:48:35 +0900

    add typeBasic

---
+++ typeBasic/typeBasic/Program.cs
@@ -0,0 +1,6 @@
+﻿var a = 1;
+var b = 1.1;
+var c = "Hello";
+Console.WriteLine($"{a}の型は{a.GetType().FullName}です。");
+Console.WriteLine($"{b}の型は{b.GetType().FullName}です。");
+Console.WriteLine($"{c}の型は{c.GetType().FullName}です。");
