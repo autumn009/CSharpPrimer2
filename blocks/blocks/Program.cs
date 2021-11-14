68b920f227e797b29dcdb786d4d83b1cf1e91626
Akira Kawamata <autumn@piedey.co.jp>
2021-11-14 10:49:23 +0900

    add blocks

---
+++ blocks/blocks/Program.cs
@@ -0,0 +1,6 @@
+﻿for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"i={i}の場合");
+    if (i == 0) Console.WriteLine("A"); Console.WriteLine("B");
+    if (i == 0) { Console.WriteLine("C"); Console.WriteLine("D"); }
+}
