76aaa78d563324764035df1d732dbd2402f94c61
Akira Kawamata <autumn@piedey.co.jp>
2021-11-17 08:43:18 +0900

    add statementsBasic

---
+++ statementsBasic/statementsBasic/Program.cs
@@ -0,0 +1,7 @@
+﻿// コメント
+sub(); // 自作のメソッド呼び出し
+for (int i = 0; i < 10; i++) Console.Write(i);  // forは組み込みステートメント
+Console.WriteLine("Hello, World!"); // ライブラリのメソッド呼び出し
+
+// 自作メソッド本体
+static void sub() => Console.WriteLine("Hello from sub method");
