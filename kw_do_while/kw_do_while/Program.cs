18352c2f8f29e8af00c23813232de27a0f075de2
Akira Kawamata <autumn@piedey.co.jp>
2021-11-16 10:27:43 +0900

    add kw_do_while

---
+++ kw_do_while/kw_do_while/Program.cs
@@ -0,0 +1,42 @@
+﻿int x = 1;
+
+Console.WriteLine("do文で繰り返す(その1)。");
+do
+{
+    x *= 2; // 変数xを2倍する
+    Console.Write(x);
+    Console.Write(" ");
+}
+while (x < 100);
+Console.WriteLine();
+
+x = 1;
+Console.WriteLine("while文で繰り返す(その1)。");
+while (x < 100)
+{
+    x *= 2; // 変数xを2倍する
+    Console.Write(x);
+    Console.Write(" ");
+};
+Console.WriteLine();
+
+x = 100;
+Console.WriteLine("do文で繰り返す(その2)。");
+do
+{
+    x *= 2; // 変数xを2倍する
+    Console.Write(x);
+    Console.Write(" ");
+}
+while (x < 100);
+Console.WriteLine();
+
+x = 100;
+Console.WriteLine("while文で繰り返す(その2)。");
+while (x < 100)
+{
+    x *= 2; // 変数xを2倍する
+    Console.Write(x);
+    Console.Write(" ");
+};
+Console.WriteLine();
