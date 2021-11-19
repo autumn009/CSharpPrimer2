c326d81910e27fa7236d3ce5fbeafe7561c6e6a1
Akira Kawamata <autumn@piedey.co.jp>
2021-11-19 10:50:45 +0900

    add integerTypes

---
+++ integerTypes/integerTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿sub<sbyte>("sbyte", sbyte.MaxValue, sbyte.MinValue);
+sub<short>("short", short.MaxValue, short.MinValue);
+sub<int>("int", int.MaxValue, int.MinValue);
+sub<nint>("nint", nint.MaxValue, nint.MinValue);
+sub<long>("long", long.MaxValue, long.MinValue);
+sub<byte>("byte", byte.MaxValue, byte.MinValue);
+sub<ushort>("ushort", ushort.MaxValue, ushort.MinValue);
+sub<uint>("uint", uint.MaxValue, uint.MinValue);
+sub<nuint>("nuint", nuint.MaxValue, nuint.MinValue);
+sub<ulong>("ulong", ulong.MaxValue, ulong.MinValue);
+
+static void sub<T>(string name, T max, T min) => Console.WriteLine($"名前:{name,-10} 本名:{typeof(T).FullName,-14} 最大値:{max,22} 最小値:{min,22}");
