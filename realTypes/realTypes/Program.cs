b245ab21e0e9173a5be6a015d3a413a323cdcd47
Akira Kawamata <autumn@piedey.co.jp>
2021-11-20 09:40:40 +0900

    add real Types

---
+++ realTypes/realTypes/Program.cs
@@ -0,0 +1,5 @@
+﻿sub<float>("float", float.MaxValue, float.MinValue);
+sub<double>("double", double.MaxValue, double.MinValue);
+sub<decimal>("decimal", decimal.MaxValue, decimal.MinValue);
+
+static void sub<T>(string name, T max, T min) => Console.WriteLine($"名前:{name,-10} 本名:{typeof(T).FullName,-14} 最大値:{max,32} 最小値:{min,32}");
