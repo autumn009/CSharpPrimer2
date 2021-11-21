23c4ddfa0d0f010cd516f34f27a7681762c85d24
Akira Kawamata <autumn@piedey.co.jp>
2021-11-21 09:22:32 +0900

    add kw_bool

---
+++ kw_bool/kw_bool/Program.cs
@@ -0,0 +1,14 @@
+﻿bool suki = false;
+for (int i = 3; i >= 1; i--)
+{
+    Console.WriteLine($"残り花びら{i}枚。花びらをむしります。");
+    suki = !suki;
+    if (suki)
+        Console.WriteLine("好き");
+    else
+        Console.WriteLine("嫌い");
+}
+Console.WriteLine("残り花びらなし!");
+Console.WriteLine("好き好き好き!");
+
+
