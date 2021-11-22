71659d1408326d2d639b9aa0fd62410c711110c3
Akira Kawamata <autumn@piedey.co.jp>
2021-11-23 08:29:16 +0900

    add kw_const

---
+++ kw_const/kw_const/Program.cs
@@ -0,0 +1,9 @@
+﻿const int a = 123;
+const int b = 456;
+const int c = a + b;    // OK
+//const int d = Math.Max(a, b);   // NG
+int d = Math.Max(a, b);    // OK
+Console.WriteLine($"a={a}");
+Console.WriteLine($"b={b}");
+Console.WriteLine($"c={c}");
+Console.WriteLine($"{a}と{b}の最大値は{d}");
