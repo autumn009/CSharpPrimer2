00708af4b41e470cf14d052160215b17d04e20a6
Akira Kawamata <autumn@piedey.co.jp>
2021-11-24 09:57:01 +0900

    add FourArithmeticOperations

---
+++ FourArithmeticOperations/FourArithmeticOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 10;
+int b = 3;
+double c = 10.0;
+double d = 3.0;
+Console.WriteLine($"a+b={a + b}");
+Console.WriteLine($"c+d={c + d}");
+Console.WriteLine($"a-b={a - b}");
+Console.WriteLine($"c-d={c - d}");
+Console.WriteLine($"a*b={a * b}");
+Console.WriteLine($"c*d={c * d}");
+Console.WriteLine($"a/b={a / b}");
+Console.WriteLine($"c/d={c / d}");
+Console.WriteLine($"a%b={a % b}");
+Console.WriteLine($"c%d={c % d}");
+
