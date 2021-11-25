cd2ba6d8428245bef7a5ee32521e7f16f126bb22
Akira Kawamata <autumn@piedey.co.jp>
2021-11-26 08:39:24 +0900

    add numbers

---
+++ numbers/numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿sub("123", 123);
+sub("123f", 123f);
+sub("123d", 123d);
+sub("123m", 123m);
+sub("123.0", 123.0);
+sub("123u", 123u);
+sub("123l", 123l); ;
+sub("123ul", 123ul);
+
+Console.WriteLine($"0x123: {0x123}");
+Console.WriteLine($"0b1010: {0b1010}");
+
+void sub(string name, object val )=>Console.WriteLine($"{name}: {val.GetType().FullName}");
