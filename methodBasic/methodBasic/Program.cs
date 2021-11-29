7b8474f5e1bd36cb4d464d8c642e3ff0300fcfaf
Akira Kawamata <autumn@piedey.co.jp>
2021-11-29 09:23:16 +0900

    add methodBasic

---
+++ methodBasic/methodBasic/Program.cs
@@ -0,0 +1,27 @@
+﻿hello();
+helloTo("Joe");
+helloTo("Mike");
+Console.WriteLine(calcAdd(1,2));
+helloShortForm();
+helloToShortForm("Joe");
+helloToShortForm("Mike");
+Console.WriteLine(calcAddShortForm(1, 2));
+
+void hello()
+{
+    Console.WriteLine("Hello!");
+}
+
+void helloTo(string name)
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+
+int calcAdd(int x, int y)
+{
+    return x + y;
+}
+
+void helloShortForm() => Console.WriteLine("Hello!");
+void helloToShortForm(string name) => Console.WriteLine($"Hello, {name}!");
+int calcAddShortForm(int x, int y) => x + y;
