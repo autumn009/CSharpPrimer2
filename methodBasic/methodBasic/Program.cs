4267e682e3b9997fc97a235596d77136fb5e9521
Akira Kawamata <autumn@piedey.co.jp>
2021-11-29 09:44:46 +0900

    add comment

--- methodBasic/methodBasic/Program.cs
+++ methodBasic/methodBasic/Program.cs
@@ -1,4 +1,5 @@
-﻿hello();
+﻿// メソッド呼び出し
+hello();
 helloTo("Joe");
 helloTo("Mike");
 Console.WriteLine(calcAdd(1,2));
@@ -7,6 +8,7 @@ helloToShortForm("Joe");
 helloToShortForm("Mike");
 Console.WriteLine(calcAddShortForm(1, 2));
 
+// メソッド宣言 (通常表記)
 void hello()
 {
     Console.WriteLine("Hello!");
@@ -22,6 +24,7 @@ int calcAdd(int x, int y)
     return x + y;
 }
 
+// メソッド宣言(短縮表記)
 void helloShortForm() => Console.WriteLine("Hello!");
 void helloToShortForm(string name) => Console.WriteLine($"Hello, {name}!");
 int calcAddShortForm(int x, int y) => x + y;
