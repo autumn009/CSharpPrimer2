b69b82397a9d16a006f35d976f47f0fdc4506873
Akira Kawamata <autumn@piedey.co.jp>
2021-12-03 09:56:26 +0900

    add console

---
+++ console/console/Program.cs
@@ -0,0 +1,16 @@
+﻿// いわゆるハロー・ワールド
+Console.WriteLine("Hello, World!");
+
+// 改行を伴わない出力
+Console.Write("It's");
+Console.Write(" Cool!");
+
+// 改行だけ行う
+Console.WriteLine();
+
+// 音を出す
+Console.Beep();
+
+// 1行入力する
+var s = Console.ReadLine();
+Console.WriteLine($"入力されたのは\"{s}\"");
