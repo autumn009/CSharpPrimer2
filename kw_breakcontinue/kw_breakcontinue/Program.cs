95e529c5504ad04fa6a100b92252d9707c4cfcad
Akira Kawamata <autumn@piedey.co.jp>
2021-12-05 15:03:00 +0900

    add kw_breakcontinue

---
+++ kw_breakcontinue/kw_breakcontinue/Program.cs
@@ -0,0 +1,11 @@
+﻿int x = 1;
+while (true)
+{
+    x = x * 2;
+    // xが32なら即座にループの次回に進む
+    if (x == 32) continue;
+    // xが100より大きいならループを脱出する
+    if (x > 100) break;
+    Console.WriteLine(x);
+}
+Console.WriteLine("Finished!");
