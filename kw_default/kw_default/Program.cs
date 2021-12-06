4fc875e63b1c89c0d37ce80e772ab80885afe0af
Akira Kawamata <autumn@piedey.co.jp>
2021-12-06 10:43:43 +0900

    add kw_default

---
+++ kw_default/kw_default/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int[] b = new int[1];
+int c = default(int);
+if (a == default)
+    Console.WriteLine("aはデフォルト値だよ。");
+else
+    Console.WriteLine("aはデフォルト値じゃないよ。");
+
+if (b[0] == default)
+    Console.WriteLine("b[0]はデフォルト値だよ。");
+else
+    Console.WriteLine("b[0]はデフォルト値じゃないよ。");
+
+if (c == default)
+    Console.WriteLine("cはデフォルト値だよ。");
+else
+    Console.WriteLine("cはデフォルト値じゃないよ。");
