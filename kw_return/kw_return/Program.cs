f9ec5b5fc47255b9fc5d12a38787bb093af7cf2b
Akira Kawamata <autumn@piedey.co.jp>
2021-12-08 10:08:07 +0900

    add kw_return

---
+++ kw_return/kw_return/Program.cs
@@ -0,0 +1,24 @@
+﻿var a = sub2(1);
+sub1(a);
+
+var b = sub2(2);
+sub1(b);
+
+void sub1(int x)
+{
+    if (x == 0)
+    {
+        Console.WriteLine("ありません。");
+        return;
+    }
+    Console.WriteLine($"{x}個あります。");
+}
+
+int sub2(int number)
+{
+    if (number == 1)
+    {
+        return 0;
+    }
+    return 100;
+}
