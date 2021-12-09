e51912a88a189c4041a978c216479b121b241163
Akira Kawamata <autumn@piedey.co.jp>
2021-12-10 08:55:05 +0900

    add kw_for

---
+++ kw_for/kw_for/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("昇順");
+for (int i = 0; i < 10; i++) Console.Write(i);
+Console.WriteLine();
+
+Console.WriteLine("降順");
+for (int i = 9; i >= 0; i--) Console.Write(i);
+Console.WriteLine();
+
+Console.WriteLine("無限ループ");
+for (; ; )
+{
+    if (DateTime.Now.Second % 10 == 0)
+    {
+        Console.WriteLine("強制脱出!");
+        break;
+    }
+}
+Console.WriteLine("Finished!");
