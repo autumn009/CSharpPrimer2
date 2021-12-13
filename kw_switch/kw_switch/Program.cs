ab751847eab8bdf86c881839dd95450250bccadf
Akira Kawamata <autumn@piedey.co.jp>
2021-12-13 09:59:05 +0900

    add kw_switch

---
+++ kw_switch/kw_switch/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("swich文の場合");
+for (int i = 0; i < 3; i++)
+{
+    switch (i)
+    {
+        case 0:
+            Console.WriteLine("ZERO");
+            break;
+        case 1:
+            Console.WriteLine("ONE");
+            break;
+        default:
+            Console.WriteLine("OTHERS");
+            break;
+    }
+}
+Console.WriteLine("swich式の場合");
+for (int i = 0; i < 3; i++)
+{
+    var x = i switch
+    {
+        0 => "ZERO",
+        1 => "ONE",
+        _ => "OTHERS"
+    };
+    Console.WriteLine(x);
+}
