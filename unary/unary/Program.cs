c100c7306aa03a94bd85e0392cfb35ca8ed2e0bd
Akira Kawamata <autumn@piedey.co.jp>
2021-12-15 13:38:24 +0900

    add unary

---
+++ unary/unary/Program.cs
@@ -0,0 +1,9 @@
+﻿var a = 123;
+var b = false;
+Console.WriteLine($"a=={a}");
+Console.WriteLine($"b=={b}");
+Console.WriteLine($"+a=={+a}");
+Console.WriteLine($"-a=={-a}");
+Console.WriteLine($"!b=={!b}");
+
+
