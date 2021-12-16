bcd408c3490bcc8ec75384bf11c785f9da8968f5
Akira Kawamata <autumn@piedey.co.jp>
2021-12-16 10:31:10 +0900

    add conditionalLogical

---
+++ conditionalLogical/conditionalLogical/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Case A");
+if (test(false, "A1") && test(true, "A2")) Console.WriteLine("True"); else Console.WriteLine("False");
+Console.WriteLine("Case B");
+if (test(true, "B1") && test(true, "B2")) Console.WriteLine("True"); else Console.WriteLine("False");
+Console.WriteLine("Case C");
+if (test(false, "C1") || test(true, "C2")) Console.WriteLine("True"); else Console.WriteLine("False");
+Console.WriteLine("Case D");
+if (test(true, "D1") || test(true, "D2")) Console.WriteLine("True"); else Console.WriteLine("False");
+
+bool test(bool x, string label)
+{
+    Console.WriteLine($"{label}が実行されました。");
+    return x;
+}
+
+
