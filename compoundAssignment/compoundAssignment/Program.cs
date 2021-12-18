0827888997557a307dbf42610f060732dbb814d7
Akira Kawamata <autumn@piedey.co.jp>
2021-12-19 08:39:59 +0900

    fix names

--- compoundAssignment/compoundAssignment/Program.cs
+++ compoundAssignment/compoundAssignment/Program.cs
@@ -1,8 +1,8 @@
-﻿int c = 1;
-int d = 2;
-c = c + d;
-Console.WriteLine($"c={c}");
-int a = 1;
+﻿int a = 1;
 int b = 2;
-a += b;
+a = a + b;
 Console.WriteLine($"a={a}");
+int c = 1;
+int d = 2;
+c += d;
+Console.WriteLine($"c={c}");
