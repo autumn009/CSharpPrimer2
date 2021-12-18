6745687946ccc96f7d9e2fdcae0d168f144c67bf
Akira Kawamata <autumn@piedey.co.jp>
2021-12-18 09:49:31 +0900

    add conditionalOperator

---
+++ conditionalOperator/conditionalOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("条件演算子で書く");
+for (int i = 0; i < 4; i++)
+{
+    var label = i % 2 == 0 ? "偶数" : "奇数";
+    Console.WriteLine($"{i}は{label}です。");
+}
+Console.WriteLine("if文で書く");
+for (int i = 0; i < 4; i++)
+{
+    string label;
+    if (i % 2 == 0)
+        label = "偶数";
+    else
+        label = "奇数";
+    Console.WriteLine($"{i}は{label}です。");
+}
