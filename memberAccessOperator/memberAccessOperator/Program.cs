af4d75ae7ffbfbd4b8890b03baf3b9e2abad8c81
Akira Kawamata <autumn@piedey.co.jp>
2021-12-20 09:16:08 +0900

    add memberAccessOperator

---
+++ memberAccessOperator/memberAccessOperator/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("文字列合体!");
+string[] array = new string[] { "ONE", "TWO", "THREE" };
+var r1 = string.Join(",", array);
+Console.WriteLine(r1);
+
+Console.WriteLine("文字列分離!");
+string[] r2 = r1.Split(',');
+foreach (var item in r2) Console.WriteLine($"{item}");
