753da3bdc89e99da59cf29fe864526edde26e113
Akira Kawamata <autumn@piedey.co.jp>
2021-12-22 08:15:26 +0900

    add kw_is

---
+++ kw_is/kw_is/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 123;
+double b = 123;
+string c = "Hello!";
+sub(a);
+sub(b);
+sub(c);
+
+void sub(object x)
+{
+    Console.Write($"x={x}: ");
+    if (x is int)
+        Console.WriteLine("それは整数型(int型)です。");
+    else
+        Console.WriteLine("それは私の知らない型です。");
+}
