ae2809631d8053d1b86cedbf05c1a013a4cb5967
Akira Kawamata <autumn@piedey.co.jp>
2021-12-23 08:14:54 +0900

    add kw_as_cast

---
+++ kw_as_cast/kw_as_cast/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("キャストの利用例"); 
+byte a=1;
+byte b=2;
+byte c = (byte)(a + b);
+Console.WriteLine($"a+b={c}");
+
+Console.WriteLine("as演算子の利用例");
+object s = "hello!";
+string? t = s as string;
+if (t != null) Console.WriteLine($"upper letter of s={t.ToUpper()}");
