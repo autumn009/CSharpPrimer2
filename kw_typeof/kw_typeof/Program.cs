802d8cb1d0a082823d22d9ab736c32d3e9c75575
Akira Kawamata <autumn@piedey.co.jp>
2021-12-24 08:39:02 +0900

    add kw_typeof

---
+++ kw_typeof/kw_typeof/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine($"1.GetType().FullName = {1.GetType().FullName}");
+Console.WriteLine($"typeof(int).FullName = {typeof(int).FullName}");
+
+Console.WriteLine("Tがintの場合");
+sub<int>();
+Console.WriteLine("Tがstringの場合");
+sub<string>();
+
+void sub<T>()
+{
+    Console.WriteLine($"typeof(T).FullName = {typeof(T).FullName}");
+}
