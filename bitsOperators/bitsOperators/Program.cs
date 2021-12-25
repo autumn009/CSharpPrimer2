bfb2c7fcb0098739ec9123f7d8e0194284384777
Akira Kawamata <autumn@piedey.co.jp>
2021-12-25 11:01:05 +0900

    add bitsOperators

---
+++ bitsOperators/bitsOperators/Program.cs
@@ -0,0 +1,8 @@
+﻿uint a = 3;
+uint b = 5;
+Console.WriteLine($"~a={~a}");
+Console.WriteLine($"a & b={a & b}");
+Console.WriteLine($"a | b={a | b}");
+Console.WriteLine($"a ^ b={a ^ b}");
+Console.WriteLine($"a << 1={a << 1}");
+Console.WriteLine($"a >> 1={a >> 1}");
