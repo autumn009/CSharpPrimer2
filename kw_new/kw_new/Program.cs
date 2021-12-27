b6cfba1e52bff372fce4a70448590ca9f2f628d7
Akira Kawamata <autumn@piedey.co.jp>
2021-12-27 09:56:12 +0900

    add kw_new

---
+++ kw_new/kw_new/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("参照型の場合");
+var s = new string('X', 10);
+Console.WriteLine($"s={s}");
+
+Console.WriteLine("値型の場合");
+var d = new DateTime(2029, 1, 1, 8, 30, 0);
+Console.WriteLine($"d={d}");
