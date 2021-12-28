9917b78084905f2675c95c2bf41aced8a371f0d8
Akira Kawamata <autumn@piedey.co.jp>
2021-12-28 09:02:38 +0900

    add assignmentOperator

---
+++ assignmentOperator/assignmentOperator/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[1];
+array[0] = 123;
+Console.WriteLine($"array[0]={array[0]}");
+int x;
+int[] y;
+// 値型の代入
+x = array[0];
+// 参照型の代入
+y = array;
+Console.WriteLine("値の更新　(参照は更新しない)");
+array[0] = 456;
+Console.WriteLine($"array[0]={array[0]}");
+Console.WriteLine($"x={x}");
+Console.WriteLine($"y={y[0]}");
