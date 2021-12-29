ef2703d3e4a8516f311f9dd0a2ff0ce46bfe2a2b
Akira Kawamata <autumn@piedey.co.jp>
2021-12-29 11:30:09 +0900

    add nullable

---
+++ nullable/nullable/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("ボックス化とボックス化解除");
+int a = 123;
+// ボックス化
+object o = a;
+// ボックス化解除
+int b = (int)o;
+Console.WriteLine($"b={b}");
+
+Console.WriteLine("null 許容値型");
+int? c = 123;
+c = null;
+c = 456;
+Console.WriteLine($"c={c}");
+
+Console.WriteLine("null 許容参照型");
+string? s = "the Jedi";
+s = null;
+s = "Return of the Jedi";
+Console.WriteLine($"s={s}");
