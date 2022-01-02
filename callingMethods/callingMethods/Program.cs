d5b92c09760784e1b058ac8f1c89aa42c8b64fa3
Akira Kawamata <autumn@piedey.co.jp>
2022-01-02 11:19:54 +0900

    add callingMethods

---
+++ callingMethods/callingMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿myMethod("普通に呼ぶぞ");
+
+Action<string> myVar = myMethod;
+myVar("変数経由で呼ぶぞ");
+
+var myArray = new Action<string>[] { myMethod };
+myArray[123 + 456 - 579]("配列経由で呼ぶぞ");
+
+void myMethod(string message)
+{
+    Console.WriteLine($"myMethod called: {message}");
+}
