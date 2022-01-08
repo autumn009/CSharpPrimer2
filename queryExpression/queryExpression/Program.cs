0ff167b7bad4a78191f45591d4131d672464a68d
Akira Kawamata <autumn@piedey.co.jp>
2022-01-08 15:33:51 +0900

    add queryExpression

---
+++ queryExpression/queryExpression/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] array = { "Alice", "Betty", "Aiko", "Cindy", "Abbie" };
+
+Console.WriteLine("クエリ式の例");
+var q1 = from n in array where n.StartsWith("A") orderby n select n.ToUpper();
+foreach (var item in q1) Console.WriteLine(item);
+
+Console.WriteLine("メソッド形式の例");
+var q2 = array.Where(c=>c.StartsWith("A")).OrderBy(c=>c).Select(c=>c.ToUpper());
+foreach (var item in q2) Console.WriteLine(item);
