28ecb46a7a87fb47b9f88fc4c0dcbb8f7caa96b2
Akira Kawamata <autumn@piedey.co.jp>
2022-01-10 10:32:49 +0900

    add kw_yield

---
+++ kw_yield/kw_yield/Program.cs
@@ -0,0 +1,15 @@
+﻿foreach (var item in DoubleCount())
+{
+    Console.WriteLine(item);
+}
+
+IEnumerable<int> DoubleCount()
+{
+    int a = 1;
+    for(; ; )
+    {
+        if (a > 100) yield break;
+        yield return a;
+        a *= 2;
+    }
+}
