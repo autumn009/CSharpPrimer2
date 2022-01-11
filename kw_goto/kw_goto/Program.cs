f20bdf08bc7ea54842fb327c9e7a4e0a52d338ff
Akira Kawamata <autumn@piedey.co.jp>
2022-01-11 09:25:29 +0900

    add kw_goto

---
+++ kw_goto/kw_goto/Program.cs
@@ -0,0 +1,13 @@
+﻿for (int x = 0; x < 10; x++)
+{
+    for (int y = 0; y < 10; y++)
+    {
+        Console.Write($"x={x},y={y} ");
+        if (x * y > 20) goto exitLabel;
+    }
+}
+// 以下の1行は永遠に実行されない
+Console.WriteLine("Loop End");
+// 以下の1行はexitLabelという名前のラベル
+exitLabel:
+Console.WriteLine("Done");
