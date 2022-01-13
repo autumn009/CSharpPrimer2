5fd1528d37f9cdbccbd686106ddb057daf707d72
Akira Kawamata <autumn@piedey.co.jp>
2022-01-13 09:13:25 +0900

    fix indent

--- accessModifiers/accessModifiers/Program.cs
+++ accessModifiers/accessModifiers/Program.cs
@@ -1,16 +1,16 @@
-﻿    var x = new Sample();
-    x.SubPublic();   // 呼べる
-    //x.SubPrivate();  // 呼べない
+﻿var x = new Sample();
+x.SubPublic();   // 呼べる
+//x.SubPrivate();  // 呼べない
 
-    public class Sample
+public class Sample
+{
+    public void SubPublic()
     {
-        public void SubPublic()
-        {
-            Console.WriteLine("pubic method");
-            SubPrivate();   // 呼べる
-        }
-        private void SubPrivate()
-        {
-            Console.WriteLine("private method");
-        }
+        Console.WriteLine("pubic method");
+        SubPrivate();   // 呼べる
     }
+    private void SubPrivate()
+    {
+        Console.WriteLine("private method");
+    }
+}
