f8952a081693f3e810abf74990d320d81cc07581
Akira Kawamata <autumn@piedey.co.jp>
2022-01-14 10:30:00 +0900

    add Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿var papa = new Person();
+papa.Name = "ウルトラの父";
+
+var boku = new Student();
+boku.Name = "ウルトラのタロウ";
+boku.SchoolName = "ウルトラ小学校";
+
+var man = new SuperMan();
+man.Name = "スーパー紅茶マン";
+man.CameFrom = "リプトン星";
+
+outputCommon(papa);
+outputCommon(boku);
+outputCommon(man);
+
+void outputCommon(Person p)
+{
+    Console.WriteLine($"あいつの名前は{p.Name}");
+}
+
+public class Person
+{
+    public string? Name { get; set; }
+}
+
+// まだまだここから継承できる
+public class Student: Person
+{
+    public string? SchoolName;
+}
+
+// もうここから継承できない
+public sealed class SuperMan : Person
+{
+    public string? CameFrom;
+}
