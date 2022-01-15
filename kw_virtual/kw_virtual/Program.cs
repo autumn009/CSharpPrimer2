e6fcf9d3fdf594c91c9878d2ce374881d1b0538e
Akira Kawamata <autumn@piedey.co.jp>
2022-01-15 09:09:46 +0900

    add kw_virtual

---
+++ kw_virtual/kw_virtual/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("BクラスのインスタンスをクラスAから使った時。");
+A x = new B();
+x.Sub1();
+x.Sub2();
+Console.WriteLine("BクラスのインスタンスをクラスBから使った時。");
+B y = new B();
+y.Sub1();
+y.Sub2();
+
+public class A
+{
+    public void Sub1()
+    {
+        Console.WriteLine("I'M A");
+    }
+    public virtual void Sub2()
+    {
+        Console.WriteLine("I'M A");
+    }
+}
+
+public class B:A
+{
+    public new void Sub1()
+    {
+        Console.WriteLine("I'M B");
+    }
+    public override void Sub2()
+    {
+        Console.WriteLine("I'M B");
+    }
+}
