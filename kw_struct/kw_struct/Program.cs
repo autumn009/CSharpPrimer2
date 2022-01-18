8d5d89a79fefe6bfedc0edd251c7fab855298e6e
Akira Kawamata <autumn@piedey.co.jp>
2022-01-18 09:23:33 +0900

    add kw_struct

---
+++ kw_struct/kw_struct/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("コンストラクタが実行されるとき");
+MyStruct a = new MyStruct(1);
+Console.WriteLine(a.X);
+
+Console.WriteLine("コンストラクタが実行されないとき");
+MyStruct[] b = new MyStruct[1];
+Console.WriteLine(b[0].X);
+
+public struct MyStruct
+{
+    public int X { get; set; } = 1;
+    public MyStruct()
+    {
+        Console.WriteLine("コンストラクタが実行されました。");
+    }
+    public MyStruct(int n)
+    {
+        Console.WriteLine("引数のあるコンストラクタが実行されました。");
+        Console.WriteLine($"メンバーXを{n}にします。");
+        X = n;
+    }
+}
