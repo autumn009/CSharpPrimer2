95b73e038b6bc24ec9c7557fc115ad08b599387d
Akira Kawamata <autumn@piedey.co.jp>
2022-01-20 09:13:04 +0900

    add kw_record

---
+++ kw_record/kw_record/Program.cs
@@ -0,0 +1,8 @@
+﻿var taro = new Person("山田", "太郎");
+Console.WriteLine($"{taro.FirstName}{taro.LastName}");
+// 以下はできない
+// taro.FirstName = "ウルトラマン";
+taro = taro with { FirstName="ウルトラマン" };
+Console.WriteLine($"{taro.FirstName}{taro.LastName}");
+
+public record Person(string FirstName, string LastName);
