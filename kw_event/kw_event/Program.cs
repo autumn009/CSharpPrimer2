a210e621f595ed6ff51768b274894823416b55aa
Akira Kawamata <autumn@piedey.co.jp>
2022-01-22 08:48:31 +0900

    add kw_event

---
+++ kw_event/kw_event/Program.cs
@@ -0,0 +1,24 @@
+﻿var a = new SampleClass();
+// イベントの登録
+a.OnNotify += (sender, e) =>
+{
+    Console.WriteLine($"オブジェクトaでイベント発生! 理由:{e.Message}");
+};
+a.SampleMethod();
+
+// イベントの情報を持つクラス
+public class MyEventArgs : EventArgs
+{
+    public string? Message { get; set; }
+}
+
+// イベントを持つクラス
+public class SampleClass
+{
+    public event EventHandler<MyEventArgs>? OnNotify;
+    public void SampleMethod()
+    {
+        // イベント発生
+        if (OnNotify != null) OnNotify(this, new MyEventArgs() { Message = "SampleMethodが呼ばれました" }) ;
+    }
+}
