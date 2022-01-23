b3dacc76fb91847ec9a3bc5f5d14209e7ff70bf0
Akira Kawamata <autumn@piedey.co.jp>
2022-01-23 09:35:49 +0900

    add kw_static

---
+++ kw_static/kw_static/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(MyStaticSample.MyPropertyStatic);
+MyStaticSample.SetPropertyToStatic(2);
+Console.WriteLine(MyStaticSample.MyPropertyStatic);
+
+public class MyStaticSample
+{
+    // 静的なプロパティ
+    public static int MyPropertyStatic { get; set; }
+    // 静的なメソッド
+    public static void SetPropertyToStatic(int x)
+    {
+        MyPropertyStatic = x;
+    }
+    // 静的なコンストラクタ
+    static MyStaticSample()
+    {
+        MyPropertyStatic = 1;
+    }
+}
