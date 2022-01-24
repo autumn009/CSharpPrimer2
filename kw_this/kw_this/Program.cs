f16b959417bf6d6aff220f0a67b3847ec68c3122
Akira Kawamata <autumn@piedey.co.jp>
2022-01-24 11:28:05 +0900

    add kw_this

---
+++ kw_this/kw_this/Program.cs
@@ -0,0 +1,19 @@
+﻿var a1 = new MySample();
+Console.WriteLine(a1.theValue);
+var a2 = new MySample(2);
+Console.WriteLine(a2.theValue);
+
+class MySample
+{
+    public int theValue { get; set; }
+    public MySample(int theValue)
+    {
+        this.theValue = theValue;
+        Console.WriteLine("コンストラクタ1号");
+    }
+    public MySample() : this(1)
+    {
+        Console.WriteLine("コンストラクタ2号");
+    }
+}
+
