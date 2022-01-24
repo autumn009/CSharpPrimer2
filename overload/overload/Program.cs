a91237642343f063af04d1f1c38979a56de271bd
Akira Kawamata <autumn@piedey.co.jp>
2022-01-25 08:47:40 +0900

    add obverload

---
+++ overload/overload/Program.cs
@@ -0,0 +1,31 @@
+﻿var x = new MySample();
+x.SetMessage();
+Console.WriteLine(x.MyMessage);
+x.SetMessage(1);
+Console.WriteLine(x.MyMessage);
+x.SetMessage(123,456);
+Console.WriteLine(x.MyMessage);
+x.SetMessage("Hello World");
+Console.WriteLine(x.MyMessage);
+
+
+class MySample
+{
+    public string MyMessage { get; set; }
+    public void SetMessage()
+    {
+        MyMessage = "ハロー";
+    }
+    public void SetMessage(int x)
+    {
+        MyMessage = $"x={x}";
+    }
+    public void SetMessage(int x, int y)
+    {
+        MyMessage = $"x={x} y={y}";
+    }
+    public void SetMessage(string message)
+    {
+        MyMessage = message;
+    }
+}
