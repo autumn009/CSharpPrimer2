2adedc7b79fa3714a14b34305dd982c381bfc28d
Akira Kawamata <autumn@piedey.co.jp>
2022-01-27 09:12:18 +0900

    add kw_dynamic

---
+++ kw_dynamic/kw_dynamic/Program.cs
@@ -0,0 +1,17 @@
+﻿sub(new A());
+sub(new B());
+
+void sub(dynamic p)
+{
+    p.Say("HELLO!");
+}
+
+class A
+{
+    public void Say(string message) => Console.WriteLine($"{message} from A");
+}
+
+class B
+{
+    public void Say(string message) => Console.WriteLine($"{message} from B");
+}
