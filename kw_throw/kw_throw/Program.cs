a4bf4cc4a91fd5fef4c47b408d63866d217540fe
Akira Kawamata <autumn@piedey.co.jp>
2022-01-28 10:35:34 +0900

    add kw_throw

---
+++ kw_throw/kw_throw/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = 1;
+for (int i = 0; i < 8; i++)
+{
+    if (x > 100) throw new MySampleException("xが100を超えました。");
+    x *= 2;
+}
+
+[Serializable]
+public class MySampleException : Exception
+{
+    public MySampleException() : base() { }
+    public MySampleException(string message) : base(message) { }
+    public MySampleException(string message, Exception inner) : base(message, inner) { }
+
+    // A constructor is needed for serialization when an
+    // exception propagates from a remoting server to the client.
+    protected MySampleException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
