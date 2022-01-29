677b54d1afefbbb6b85eea8c032fe9f7718267f0
Akira Kawamata <autumn@piedey.co.jp>
2022-01-30 08:38:00 +0900

    add kw_namespace

---
+++ kw_namespace/kw_namespace/Program.cs
@@ -0,0 +1,28 @@
+﻿using NamespaceBeta;
+
+var x = new NamespaceAlpha.MySample();
+x.Sub();
+var y = new MySample();
+y.Sub();
+
+namespace NamespaceAlpha
+{
+    public class MySample
+    {
+        public void Sub()
+        {
+            Console.WriteLine("I'm NamespaceAlpha.MySample.Sub()");
+        }
+    }
+}
+
+namespace NamespaceBeta
+{
+    public class MySample
+    {
+        public void Sub()
+        {
+            Console.WriteLine("I'm NamespaceBeta.MySample.Sub()");
+        }
+    }
+}
