1435f16560e4663fa44ad662786ca492e741dc02
Akira Kawamata <autumn@piedey.co.jp>
2022-02-01 09:19:11 +0900

    add kw_partial

---
+++ kw_partial/kw_partial/Program.cs
@@ -0,0 +1,27 @@
+﻿var x = new A();
+x.PublicSub();
+
+partial class A
+{
+    private void privateSub()
+    {
+        Console.WriteLine("A.privateSub");
+    }
+    partial void PartialMethodA();
+    partial void PartialMethodB();
+}
+
+partial class A
+{
+    public void PublicSub()
+    {
+        Console.WriteLine("A.PublicSub");
+        privateSub();
+        PartialMethodA();
+        PartialMethodB();
+    }
+    partial void PartialMethodA()
+    {
+        Console.WriteLine("A.PartialMethodA");
+    }
+}
