4960a47b21aad8efc7d554a4548979d917590ac3
Akira Kawamata <autumn@piedey.co.jp>
2022-02-04 10:45:49 +0900

    add extensionMethods

---
+++ extensionMethods/extensionMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿string s = "オレのおじさん";
+Console.WriteLine($"変換前: {s}");
+Console.WriteLine($"変換後: {s.ToOrego()}");
+
+public static class MyExtensions
+{
+    public static string ToOrego(this string str)
+    {
+        return str.Replace("オ", "レレ");
+    }
+}
---
+++ kw_base/kw_base/Program.cs
@@ -0,0 +1,22 @@
+﻿var x = new MyClassB();
+
+class MyClassA
+{
+    public virtual void Sub() => Console.WriteLine("A.Sub called");
+    public MyClassA(string messasge) => Console.WriteLine($"MyClassA's constructor called: {messasge}");
+}
+
+class MyClassB : MyClassA
+{
+    public override void Sub() => Console.WriteLine("B.Sub called");
+    public MyClassB() : base("from MyClassB")
+    {
+        Console.WriteLine("MyClassB's constructor called");
+        // 無指定のアクセス
+        Sub();
+        // thisアクセス
+        this.Sub();
+        // baseアクセス
+        base.Sub();
+    }
+}
