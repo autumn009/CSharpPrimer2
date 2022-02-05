6008935f40f722d51fcff28139504fb9bf1de002
Akira Kawamata <autumn@piedey.co.jp>
2022-02-05 09:11:38 +0900

    add kw_async

---
+++ kw_async/kw_async/Program.cs
@@ -0,0 +1,12 @@
+﻿var r = await sub(123,456);
+Console.WriteLine($"計算結果: {r}");
+
+async Task<int> sub(int x,int y)
+{
+    await Console.Out.WriteLineAsync("計算中...");
+    return (x + y) / 2;
+}
+
+
+
+
