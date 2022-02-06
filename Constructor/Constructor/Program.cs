85188d7c5bbf0687115202007fd05f1aa72120b6
Akira Kawamata <autumn@piedey.co.jp>
2022-02-06 11:27:55 +0900

    add Constructor

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,13 @@
+﻿var a = new MySample("サンプル実行");
+
+class MySample
+{
+    public MySample(string message)
+    {
+        Console.WriteLine($"MySampleは生成されます。:{message}");
+    }
+    ~MySample()
+    {
+        Console.WriteLine("MySampleは破棄されます。");
+    }
+}
