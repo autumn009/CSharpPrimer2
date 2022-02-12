110263219a440e91f62ddc986aa9a872dd568ba2
Akira Kawamata <autumn@piedey.co.jp>
2022-02-12 19:05:14 +0900

    add kw_operator

---
+++ kw_ operator/kw_ operator/Program.cs
@@ -0,0 +1,21 @@
+﻿var a = new A() { Name = "Taro" };
+B b = a;
+C c = (C)a;
+Console.WriteLine(b.Name);
+Console.WriteLine(c.Name);
+
+public class A
+{
+    public string? Name { get; set; }
+}
+public class B
+{
+    public string? Name { get; set; }
+    public static implicit operator B(A a) => new B() { Name = a.Name };
+}
+
+public class C
+{
+    public string? Name { get; set; }
+    public static explicit operator C(A a) => new C() { Name = a.Name };
+}
