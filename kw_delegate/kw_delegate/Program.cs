01fef198dd180ea7851d5992d34f4c831eed4d76
Akira Kawamata <autumn@piedey.co.jp>
2022-02-12 14:33:20 +0900

    add kw_delegate

---
+++ kw_delegate/kw_delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿sub((x, y) => (x + y).ToString());
+subF((x, y) => (x + y).ToString());
+
+// 自分で定義したデリゲート型を使った時
+void sub(TwoInt proc)
+{
+    Console.WriteLine(proc(1, 2));
+}
+// 定義済みのFunc型を使ったとき
+void subF(Func<int,int, string> proc)
+{
+    Console.WriteLine(proc(1, 2));
+}
+
+// デリゲート型の宣言
+delegate string TwoInt(int x, int y);
