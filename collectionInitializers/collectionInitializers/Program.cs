eb46c16d6736001059ab73f45c06a54fd956b02a
Akira Kawamata <autumn@piedey.co.jp>
2022-02-15 15:21:35 +0900

    add collectionInitializers

---
+++ collectionInitializers/collectionInitializers/Program.cs
@@ -0,0 +1,14 @@
+﻿var list= new List<int> { 123, 456, 789 };
+foreach (var item in list) Console.WriteLine(item);
+var dic1 = new Dictionary<string, string> 
+{
+    {"Key1","value1" },
+    {"Key2","value2" }
+};
+foreach (var item in dic1.Keys) Console.WriteLine($"{item}={dic1[item]}");
+var dic2 = new Dictionary<string, string>
+{
+    ["Key1"] = "value1",
+    ["Key2"] = "value2",
+};
+foreach (var item in dic2.Keys) Console.WriteLine($"{item}={dic2[item]}");
---
+++ kw_task/kw_task/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Task.Run");
+var task = Task.Run(async () =>
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(i);
+        await Task.Delay(500);
+    }
+});
+foreach (var item in "ABCDEF")
+{
+    Console.Write(item);
+    await Task.Delay(300);
+}
+await task;
+Console.WriteLine();
+Console.WriteLine("Parallel.ForEachAsync");
+
+await Parallel.ForEachAsync(new int[] {0,1,2,3,4,5,6,7,8,9}, new ParallelOptions(), async (number,token) =>
+{
+    await Task.Delay(Random.Shared.Next(1000));
+    Console.Write(number);
+});
