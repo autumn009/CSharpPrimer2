29ab5b703304a26c76fa8645b6892218fd02f7ff
Akira Kawamata <autumn@piedey.co.jp>
2022-02-15 19:13:26 +0900

    add objectInitializers

---
+++ objectInitializers/objectInitializers/Program.cs
@@ -0,0 +1,13 @@
+﻿var papa = new SamplePerson() { Name = "Kenji", Age = 40 };
+// ↓できない　(initだから)
+// papa.Name = "Kentaro";
+// ↓できる　(setだから)
+papa.Age = 41;
+Console.WriteLine($"{papa.Name}は{papa.Age}歳");
+
+class SamplePerson
+{
+    public string? Name { get; init; }
+    public int Age { get; set; }
+}
+
