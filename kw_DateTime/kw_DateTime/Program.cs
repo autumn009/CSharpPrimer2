3f3db2933d658cea743fdf90338c970f422b3e18
Akira Kawamata <autumn@piedey.co.jp>
2022-02-16 12:01:56 +0900

    add kw_DateTime

---
+++ kw_DateTime/kw_DateTime/Program.cs
@@ -0,0 +1,7 @@
+﻿var d = new DateTime(2030, 1, 2, 12, 0, 0); // 2030年1月2日12時0分0秒
+d = d.AddMinutes(30);   // 30分加算
+Console.WriteLine(d);
+Console.WriteLine(d.ToLongDateString());
+Console.WriteLine(d.ToLongTimeString());
+Console.WriteLine(d.ToShortDateString());
+Console.WriteLine(d.ToShortTimeString());
