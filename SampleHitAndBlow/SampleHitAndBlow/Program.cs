d1c00f010db35deeedcb16ea834efe83372097a3
Akira Kawamata <autumn@piedey.co.jp>
2022-02-16 19:13:39 +0900

    add SampleHitAndBlow

---
+++ SampleHitAndBlow/SampleHitAndBlow/Program.cs
@@ -0,0 +1,14 @@
+﻿int target = Random.Shared.Next(9) + 1;
+
+for (; ; )
+{
+    Console.WriteLine("1から10までの数を当てて下さい。");
+    var val = int.Parse(Console.ReadLine() ?? "");
+    if (target > val)
+        Console.WriteLine("小さすぎます");
+    else if (target < val)
+        Console.WriteLine("大ぎます");
+    else
+        break;
+}
+Console.WriteLine("当たりです。");
